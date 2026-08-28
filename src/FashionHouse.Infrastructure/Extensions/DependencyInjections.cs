66ee690fc5b3378edb0e918bb8d71a9e0e5ec5fb
mdariful79 <mdariful057@gmail.com>
2026-08-29 01:30:38 +0600

    1. Added > Project References. DependencyInjections, WebModule 2. Integrate > Serilog, Autofac

---
+++ src/FashionHouse.Infrastructure/Extensions/DependencyInjections.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FashionHouse.Infrastructure.Extensions
+{
+    public static class DependencyInjections
+    {
+        public static IServiceCollection AddInfrastructureDependency(this IServiceCollection services)
+        {
+            
+            return services;
+        }
+    }
+}
+ 
---
+++ src/FashionHouse.Web/WebModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+
+namespace FashionHouse.Web
+{
+    public class WebModule : Module
+    {
+        private readonly string _connectionString;
+
+        public WebModule(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+        }
+    }
+}
+
